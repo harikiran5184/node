8eb8a663c695f05bdbb77bc2de423f22d3dec5cc
Hari Kiran <113755124+harikiran5184@users.noreply.github.com>
2023-11-25 22:16:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app=express()
 
 const cors=require('cors')
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit:'30mb'}));
 const mongoose=require('mongoose')
 
 mongoose.connect('mongodb+srv://hari:hari@cluster0.1socvoq.mongodb.net/',{dbName:"project"}).then(()=>{console.log("Db connected")}).catch((err)=>{console.log("Error Occured")})
 
 const Login = require('./models/login')
+const profilePhoto=require('./models/profile')
 //const Signup=require('./models/signup')
 app.post('/login',async (req,res)=>{
     const {username,password}=req.body
@@ -43,6 +44,39 @@ app.post('/signup',async (req,res)=>{
     }
     catch(e){
         console.log("signup error",e)
+    }
+})
+app.post('/profilePhoto',async (req,res)=>{
+    const {userName,phone,photo}=req.body
+    console.log(userName)
+    try{
+        let check=await profilePhoto.find({phoneno:phone})
+        if(check.length>0){
+            const result=await profilePhoto.updateOne({phoneno:phone},{$set:{photo:photo}})
+            console.log(result)
+            res.json("updated")
+        }
+        else{
+        let data=new profilePhoto({phoneno:phone,userName:userName,photo:photo})
+        await data.save();
+        res.json("Updated")
+        }
+    }
+    catch(e){
+        res.json("update failed")
+    }
+})
+app.post('/getProfilePhoto',async (req,res)=>{
+    const {phone}=req.body
+
+    try{
+        let check=await profilePhoto.find({phoneno:phone})
+        if(check.length>0){
+            res.json(check[0].photo)
+        }
+    }
+    catch{
+
     }
 })
 
